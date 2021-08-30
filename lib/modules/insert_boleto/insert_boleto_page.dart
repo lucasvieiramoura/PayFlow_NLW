8146398c88e796ae20bb29ee3f55f712126ced7a
lucasvieiramoura <lucasvieiramoura@gmail.com>
2021-08-30 18:55:33 -0300

    TextFromField

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -15,7 +17,21 @@ class InsertBoletoPage extends StatelessWidget {
           color: AppColors.input,
         ),
       ),
-      body: Title(color: Colors.black, child: Text("Deu Certo")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
+            child: Text("Preecha os dados do boleto",
+                style: TextStyles.titleBoldHeading,
+                textAlign: TextAlign.center),
+          ),
+          InputTextWidget(
+            label: "Nome do Boleto",
+            icon: Icons.design_services_outlined,
+          ),
+        ],
+      ),
     );
   }
 }
